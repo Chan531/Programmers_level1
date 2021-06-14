38843f3a50d40fc48a0e0f4550ae29757fc08e59
Chan531 <chan657312@gmail.com>
2021-06-14 12:55:44 +0900

    프로그래머스 1단계 문자열을 정수로 만들기

---
+++ 문자열을정수로만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
+
+void main() {
+    cout << solution("-1234");
+}
